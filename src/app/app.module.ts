58b2c134286d0df6e96dfcc5e7b0838db994bfed
Willian LessLoss <w.felipebraz@gmail.com>
2023-12-05 12:55:35 -0300

    página de search parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SearchPageComponent } from './search-page/search-page.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { SearchPageComponent } from './search-page/search-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { AlbumService } from '../services/songsApis/searchAlbunsAPI.service';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styleUrl: './search-page.component.css'
+  styleUrl: './search-page.component.css',
 })
 export class SearchPageComponent {
-
+  artist: string = '';
+  constructor(private searchService: AlbumService) {}
+  searchAlbums(artist: string) {
+    console.log('aqui o artist:', this.artist);
+    this.searchService.searchAlbumsAPI(artist).subscribe();
+  }
 }
