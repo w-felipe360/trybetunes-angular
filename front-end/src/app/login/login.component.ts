82c45e67f613da271d7bfaf7fa4f42c41504e72b
Willian LessLoss <w.felipebraz@gmail.com>
2023-12-21 12:41:18 -0300

    Add SweetAlert2 library for better user experience

--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { UserServicesService } from '../services/UserAPI/user-services.service';
 import { Router } from '@angular/router';
 import { LoginService } from './login.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   constructor(
@@ -14,16 +15,19 @@ export class LoginComponent {
     private router: Router,
     private loginService: LoginService
   ) {}
-  // createUser(username: string, password: string) {
-  //   this.userService
-  //     .createUser({ name: username, password: password })
-  //     .subscribe();
-  //   this.router.navigate(['/search']);
-  // }
 
-  createUser(username: string, password: string) {
-    this.loginService.login(username, password).subscribe((res) => {
-      this.router.navigate(['/search']);
+  login(username: string, password: string) {
+    this.loginService.login(username, password).subscribe({
+      next: () => {
+        this.router.navigate(['/search']);
+      },
+      error: () => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Invalid username or password',
+        });
+      },
     });
   }
 }
