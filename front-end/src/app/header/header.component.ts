cf71f697bd94e6f580fc84a0779f857778d8e47d
Willian LessLoss <w.felipebraz@gmail.com>
2023-12-27 09:24:08 -0300

    Update user image and username in header and profile components

--- front-end/src/app/header/header.component.ts
+++ front-end/src/app/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProfileService } from '../profile/profile.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  user: any;
+  constructor(private readonly profileService: ProfileService) {}
+  ngOnInit() {
+    this.profileService.getUser().subscribe((user) => {
+      this.user = user;
+    });
+  }
 }
